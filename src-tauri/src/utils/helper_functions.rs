95fa69162336e6312b5835d0a03b457c2e75af93
Etienne Collin <99756528+etiennecollin@users.noreply.github.com>
2023-07-18 23:46:32 -0400

    Added bincode package
    Wrote helper functions to use it.

--- src-tauri/src/utils/helper_functions.rs
+++ src-tauri/src/utils/helper_functions.rs
@@ -1,7 +1,8 @@
 //! Helper functions for the application.
 
 use chrono::NaiveDate;
-use std::time::SystemTime;
+use serde::{de::DeserializeOwned, Deserialize, Serialize};
+use std::{time::SystemTime, vec};
 
 /// Generates a unique id based on the current time in nanoseconds.
 /// # Panics
@@ -26,4 +27,23 @@ pub fn verify_date_format(date: String) -> String {
         Ok(_) => date.unwrap().to_string(),
         Err(_) => panic!("Invalid date"),
     }
+}
+
+/// Serializes a struct into a vector of bytes.
+/// # Panics
+/// Panics if the struct cannot be serialized into a vector of bytes.
+/// This happens if the struct does not implement the Serialize trait.
+pub fn serialize(obj: &impl serde::Serialize) -> Vec<u8> {
+    bincode::serialize(&obj).unwrap()
+}
+
+/// Deserializes a vector of bytes into a struct.
+/// # Panics
+/// Panics if the vector of bytes cannot be deserialized into the struct.
+/// This happens if the struct does not implement the Deserialize trait.
+pub fn deserialize<T>(encoded_obj: &Vec<u8>) -> T
+where
+    T: DeserializeOwned,
+{
+    bincode::deserialize(encoded_obj).unwrap()
 }
